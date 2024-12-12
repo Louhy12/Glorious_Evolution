7213a87fc14c7cd4df10956e6629faa3160f54de
Louhy12 <92329092+Louhy12@users.noreply.github.com>
2024-12-12 16:46:45 +0100

    Update script.js

--- film-reel-website/script.js
+++ film-reel-website/script.js
@@ -5,13 +5,13 @@ mapSlider.addEventListener('input', function() {
     const mapIndex = mapSlider.value;
     switch(mapIndex) {
         case '1':
-            mapImage.src = 'images/map1.jpeg';
+            mapImage.src = 'map/map1.jpeg';
             break;
         case '2':
-            mapImage.src = 'images/map2.jpeg';
+            mapImage.src = 'map/map2.jpeg';
             break;
         case '3':
-            mapImage.src = 'images/map3.jpeg';
+            mapImage.src = 'map/map3.jpeg';
             break;
     }
 });
