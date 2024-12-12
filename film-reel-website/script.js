0cf4599b5d98ff24e551d525f3266880e7edd5d2
Louhy12 <92329092+Louhy12@users.noreply.github.com>
2024-12-12 16:43:35 +0100

    Redoing

--- film-reel-website/script.js
+++ film-reel-website/script.js
@@ -8,24 +8,3 @@ function autoScroll() {
 }
 
 setInterval(autoScroll, 50);
-
-document.addEventListener("DOMContentLoaded", () => {
-    const mapSlider = document.getElementById("map-slider");
-    const mapImage = document.getElementById("map-image");
-
-    // Define the image paths
-    const mapImages = [
-        "map/map1.jpeg", // Before 1975
-        "map/map2.jpeg", // 1975-1990
-        "map/map3.jpeg", // After 1990
-    ];
-
-    // Change image based on slider value
-    mapSlider.addEventListener("input", () => {
-        const selectedIndex = mapSlider.value - 1; // Slider values start from 1
-        mapImage.src = mapImages[selectedIndex];
-    });
-});
-
-
-
