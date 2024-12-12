519829716a388ea52532c4e890f3bbf50cffce88
Louhy12 <92329092+Louhy12@users.noreply.github.com>
2024-12-12 11:02:32 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const filmReel = document.querySelector('.film-reel');
+let scrollAmount = 0;
+
+function autoScroll() {
+    scrollAmount += 2;
+    filmReel.scrollTop = scrollAmount;
+    if (scrollAmount >= filmReel.scrollHeight) scrollAmount = 0;
+}
+
+setInterval(autoScroll, 50);
