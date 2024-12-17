d8c2e249212030db2f4e7cbedead7dc882b7b4a0
Louhy12 <92329092+Louhy12@users.noreply.github.com>
2024-12-17 11:46:37 +0100

    Deleting my modifications

--- film-reel-website/script.js
+++ film-reel-website/script.js
@@ -1,6 +1,20 @@
 const mapSlider = document.getElementById('map-slider');
 const mapImage = document.getElementById('map-image');
 
+mapSlider.addEventListener('input', function() {
+    const mapIndex = mapSlider.value;
+    switch(mapIndex) {
+        case '1':
+            mapImage.src = 'map/map1.jpeg';
+            break;
+        case '2':
+            mapImage.src = 'map/map2.jpeg';
+            break;
+        case '3':
+            mapImage.src = 'map/map3.jpeg';
+            break;
+    }
+});
 
 const filmReel = document.querySelector('.film-reel');
 let scrollAmount = 0;
@@ -12,33 +26,47 @@ function autoScroll() {
 }
 
 
-// Function to initialize multiple sliders
-function initializeSliders() {
-    // Select all slider inputs
-    const sliders = document.querySelectorAll('.map-slider-input');
-
-    sliders.forEach(slider => {
-        // Get the folder path from the data-folder attribute
-        const folder = slider.dataset.folder;
-
-        // Get the associated image element by matching the slider's ID
-        const image = document.querySelector(`#${slider.id.replace('-slider', '-image')}`);
-
-        // Add event listener to update the image dynamically
-        slider.addEventListener('input', function () {
-            const commentNumber = this.value; // Current slider value
-            const imagePath = `${folder}/comment${commentNumber}.png`;
-
-            // Update image src and alt text
-            image.src = imagePath;
-            image.alt = `Comment ${commentNumber}`;
-        });
-    });
-}
-
-// Call the function to set up all sliders
-initializeSliders();
-
+mapSlider.addEventListener('input', function() {
+    const mapIndex = mapSlider.value;
+    switch(mapIndex) {
+        case '1':
+            mapImage.src = 'comments/comment1.png';
+            break;
+        case '2':
+            mapImage.src = 'comments/comment2.png';
+            break;
+        case '3':
+            mapImage.src = 'comments/comment3.png';
+            break;
+        case '4':
+            mapImage.src = 'comments/comment4.png';
+            break;
+        case '5':
+            mapImage.src = 'comments/comment5.png';
+            break;
+        case '6':
+            mapImage.src = 'comments/comment6.png';
+            break;
+        case '7':
+            mapImage.src = 'comments/comment7.png';
+            break;
+        case '8':
+            mapImage.src = 'comments/comment8.png';
+            break;
+        case '9':
+            mapImage.src = 'comments/comment9.png';
+            break;
+        case '10':
+            mapImage.src = 'comments/comment10.png';
+            break;
+        case '11':
+            mapImage.src = 'comments/comment11.png';
+            break;
+        case '12':
+            mapImage.src = 'comments/comment12.png';
+            break;
+    }
+});
 
 
 setInterval(autoScroll, 50);
