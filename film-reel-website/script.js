60b1d85a3ca1c1fc50c228249163d6f62e3679cc
Louhy12 <92329092+Louhy12@users.noreply.github.com>
2024-12-17 12:27:35 +0100

    Update script.js

--- film-reel-website/script.js
+++ film-reel-website/script.js
@@ -10,143 +10,36 @@ function autoScroll() {
 }
 
 
-mapSlider.addEventListener('input', function() {
-    const mapIndex = mapSlider.value;
-    switch(mapIndex) {
-        case '1':
-            mapImage.src = 'comments/comment1.png';
-            break;
-        case '2':
-            mapImage.src = 'comments/comment2.png';
-            break;
-        case '3':
-            mapImage.src = 'comments/comment3.png';
-            break;
-        case '4':
-            mapImage.src = 'comments/comment4.png';
-            break;
-        case '5':
-            mapImage.src = 'comments/comment5.png';
-            break;
-        case '6':
-            mapImage.src = 'comments/comment6.png';
-            break;
-        case '7':
-            mapImage.src = 'comments/comment7.png';
-            break;
-        case '8':
-            mapImage.src = 'comments/comment8.png';
-            break;
-        case '9':
-            mapImage.src = 'comments/comment9.png';
-            break;
-        case '10':
-            mapImage.src = 'comments/comment10.png';
-            break;
-        case '11':
-            mapImage.src = 'comments/comment11.png';
-            break;
-        case '12':
-            mapImage.src = 'comments/comment12.png';
-            break;
-    }
-});
+function initializeSliders() {
+    // Map of slider IDs to folder paths
+    const sliders = [
+        { sliderId: "comments-slider", imageId: "comments-image", folder: "comments" },
+        { sliderId: "diversity-slider", imageId: "diversity-image", folder: "map/diversity_score" },
+        { sliderId: "height-slider", imageId: "height-image", folder: "map/height" },
+        { sliderId: "age-slider", imageId: "age-image", folder: "map/age" },
+        { sliderId: "gender-slider", imageId: "gender-image", folder: "map/gender" },
+        { sliderId: "ethnicity-slider", imageId: "ethnicity-image", folder: "map/ethnicity" },
+        { sliderId: "foreign-slider", imageId: "foreign-image", folder: "map/foreign" }
+    ];
 
-mapSlider.addEventListener('input', function() {
-    const mapIndex = mapSlider.value;
-    switch(mapIndex) {
-        case '1':
-            mapImage.src = 'map/diversity_score/diversity1.jpeg';
-            break;
-        case '2':
-            mapImage.src = 'map/diversity_score/diversity2.jpeg';
-            break;
-        case '3':
-            mapImage.src = 'map/diversity_score/diversity3.jpeg';
-            break;
+    // Loop through each slider and attach functionality
+    sliders.forEach(({ sliderId, imageId, folder }) => {
+        const slider = document.getElementById(sliderId);
+        const image = document.getElementById(imageId);
 
-    }
-});
+        // Attach event listener to update the image dynamically
+        slider.addEventListener("input", function () {
+            const index = this.value; // Get slider value
+            const newImagePath = `${folder}/comment${index}.png`; // Adjusted for "comments" folder
+            image.src = newImagePath; // Update the image source
+            image.alt = `${folder.split('/').pop()} ${index}`; // Update the alt text
+        });
+    });
+}
 
-mapSlider.addEventListener('input', function() {
-    const mapIndex = mapSlider.value;
-    switch(mapIndex) {
-        case '1':
-            mapImage.src = 'map/height/height1.jpeg';
-            break;
-        case '2':
-            mapImage.src = 'map/height/height2.jpeg';
-            break;
-        case '3':
-            mapImage.src = 'map/height/height3.jpeg';
-            break;
+// Initialize sliders when the DOM is loaded
+document.addEventListener("DOMContentLoaded", initializeSliders);
 
-    }
-});
-
-mapSlider.addEventListener('input', function() {
-    const mapIndex = mapSlider.value;
-    switch(mapIndex) {
-        case '1':
-            mapImage.src = 'map/age/age1.jpeg';
-            break;
-        case '2':
-            mapImage.src = 'map/age/age2.jpeg';
-            break;
-        case '3':
-            mapImage.src = 'map/age/age3.jpeg';
-            break;
-
-    }
-});
-
-mapSlider.addEventListener('input', function() {
-    const mapIndex = mapSlider.value;
-    switch(mapIndex) {
-        case '1':
-            mapImage.src = 'map/gender/gender1.jpeg';
-            break;
-        case '2':
-            mapImage.src = 'map/gender/gender2.jpeg';
-            break;
-        case '3':
-            mapImage.src = 'map/gender/gender3.jpeg';
-            break;
-
-    }
-});
-
-mapSlider.addEventListener('input', function() {
-    const mapIndex = mapSlider.value;
-    switch(mapIndex) {
-        case '1':
-            mapImage.src = 'map/ethnicity/ethnicity1.jpeg';
-            break;
-        case '2':
-            mapImage.src = 'map/ethnicity/ethnicity2.jpeg';
-            break;
-        case '3':
-            mapImage.src = 'map/ethnicity/ethnicity3.jpeg';
-            break;
-
-    }
-});
-
-mapSlider.addEventListener('input', function() {
-    const mapIndex = mapSlider.value;
-    switch(mapIndex) {
-        case '1':
-            mapImage.src = 'map/foreign/foreign1.jpeg';
-            break;
-        case '2':
-            mapImage.src = 'map/foreign/foreign2.jpeg';
-            break;
-        case '3':
-            mapImage.src = 'map/foreign/foreign3.jpeg';
-            break;
-
-    }
-});
 
 setInterval(autoScroll, 50);
 
